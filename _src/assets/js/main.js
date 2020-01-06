e33661bf454697b5aaeff39f6dddef4d0edc66e6
ana-sanchez <snchz.rojo@gmail.com>
2020-01-07 00:02:53 +0100

    added images styles and name style home and menu hamburguer

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
+'use strict';
+
 console.log('>> Ready :)');
+
+var menu = document.querySelector('.hamburger');
+
+function toggleMenu(event) {
+    this.classList.toggle('is-active');
+    document.querySelector(".menuppal").classList.toggle("is_active");
+    event.preventDefault();
+}
+
+menu.addEventListener('click', toggleMenu, false);
+
