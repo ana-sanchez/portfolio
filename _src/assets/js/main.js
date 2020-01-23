a8227c3bff815ad6ee1a9c6a0a26f76a798210dc
ana-sanchez <snchz.rojo@gmail.com>
2020-01-23 21:51:58 +0100

    add filter function at the project page

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,10 +1,8 @@
 'use strict';
 
-'use strict';
-
 console.log('>> Ready :)');
 
-var menu = document.querySelector('.hamburger');
+const menu = document.querySelector('.hamburger');
 
 function toggleMenu(event) {
     this.classList.toggle('is-active');
@@ -12,5 +10,50 @@ function toggleMenu(event) {
     event.preventDefault();
 }
 
+const elementportfolio = document.querySelectorAll('.portfolio');
+const elementWeb = document.getElementById('js-web');
+const elementJs = document.getElementById('js-javascript');
+const elementReact = document.getElementById('js-react')
+const elementRicky = document.getElementById('js-ricky')
+const buttonAll = document.querySelector('#btn-all');
+const buttonWeb = document.getElementById('btn-web');
+const buttonJs = document.querySelector('#btn-js');
+const buttonReact = document.querySelector('#btn-react');
+
+function selectCategoryAll() {
+    elementWeb.classList.remove('hidden')
+    elementReact.classList.remove('hidden')
+    elementRicky.classList.remove('hidden')
+    elementJs.classList.remove('hidden')
+}
+
+
+function selectCategoryWeb() {
+   elementJs.classList.add('hidden')
+   elementReact.classList.add('hidden')
+   elementRicky.classList.add('hidden')
+   elementWeb.classList.remove('hidden')
+}
+
+function selectCategoryJs(){
+    elementJs.classList.remove('hidden')
+    elementReact.classList.add('hidden')
+    elementRicky.classList.add('hidden')
+    elementWeb.classList.add('hidden')  
+}
+
+function selectCategoryReact() {
+    elementJs.classList.add('hidden')
+    elementReact.classList.remove('hidden')
+    elementRicky.classList.remove('hidden')
+    elementWeb.classList.add('hidden')
+ }
+
+
 menu.addEventListener('click', toggleMenu, false);
+buttonAll.addEventListener('click', selectCategoryAll)
+buttonWeb.addEventListener('click', selectCategoryWeb)
+buttonJs.addEventListener('click', selectCategoryJs)
+buttonReact.addEventListener('click', selectCategoryReact)
+
 
