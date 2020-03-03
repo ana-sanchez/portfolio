82148b4c6b06808cf725f09f9b2c9c5588af7439
ana-sanchez <snchz.rojo@gmail.com>
2020-03-03 13:22:16 +0100

    add dark and light mode

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -84,10 +84,34 @@ function selectCategoryReact() {
     elementSciencie.classList.remove('hidden')
     elementCero.classList.remove('hidden')
     elementBudget.classList.add('hidden')
-
-
+    console.log('hola')
  }
 
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+  
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        console.log('hola')
+    }
+    else {document.documentElement.setAttribute('data-theme', 'light');
+          localStorage.setItem('theme', 'light');
+    }    
+}
+
+if (toggleSwitch) {
+    toggleSwitch.addEventListener('change', switchTheme, false);
+}
 
 menu.addEventListener('click', toggleMenu, false);
 buttonAll.addEventListener('click', selectCategoryAll)
@@ -95,5 +119,7 @@ buttonWeb.addEventListener('click', selectCategoryWeb)
 buttonJs.addEventListener('click', selectCategoryJs)
 buttonReact.addEventListener('click', selectCategoryReact)
 buttonWordpress.addEventListener('click' , selectCategoryWordpress)
+
+
 
 
